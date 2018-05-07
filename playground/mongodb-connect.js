5fed5083b156ea956a3ce459542df819fc2f4a81
Ashish Jain <Ashish.jain8847@gmail.com>
2018-05-07 11:04:02 +0530

    Setup App for Heroku

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,7 @@
 //const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
   if (err) {
   return console.log('Unable to connect to MONGODB server');
